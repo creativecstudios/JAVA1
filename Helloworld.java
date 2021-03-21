c2a9803492f4de8ce2474819c515ea93ab0d8a8a
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-03-21 18:51:16 +0530

    Delete Helloworld.java

--- Helloworld.java
+++
@@ -1,7 +0,0 @@
-class Helloworld {
-	public static void main(String args[]) {
-		int abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz = 23;
-		System.out.println(abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz);
-		System.out.println("HelloWorld");
-	}
-}
