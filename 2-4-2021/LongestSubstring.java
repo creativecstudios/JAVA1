6b470c8fd7edb4c7f648fe7dafc90b574c07298c
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-02 18:10:49 +0530

    Add files via upload

---
+++ 2-4-2021/LongestSubstring.java
@@ -0,0 +1,30 @@
+package Leetcode;
+import java.util.Scanner;
+
+class LongestSubstring {
+        public static int lengthOfLongestSubstring(String s) {
+        int indx1 = 0;
+        int indx2 = 0;
+        int max = 0;
+        HashSet<Character> hashset = new HashSet();
+        while(indx2 < s.length()){
+            if(! hashset.contains(s.charAt(indx2))){
+                hashset.add(s.charAt(indx2));
+                indx2++;
+                max = Math.max(hashset.size(),max);
+            }
+            else{
+                hashset.remove(s.charAt(indx1));
+                indx1++;
+            }
+        }
+        return max;
+    }
+
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		System.out.println(lengthofLongestSubstring(input));
+	}
+    
+}
