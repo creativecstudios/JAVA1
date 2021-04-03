77ae83b6dc5adc569f42547064719bfbddc53cc9
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-03 19:00:15 +0530

    Add files via upload

---
+++ 2021-04-02/Reversenum.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/reverse-integer/
+package Leetcode;
+import java.util.Scanner;
+
+class Reversenum {
+    public static int reverse(int x) {
+    
+        int result = 0;
+        int dummy = 0;
+        while(x!=0){
+            dummy = x%10;
+            x /=10;
+        if(result > Integer.MAX_VALUE/10 || result == Integer.MAX_VALUE/10 && dummy >7)return 0;
+        if(result < Integer.MIN_VALUE/10 || result == Integer.MIN_VALUE/10 && dummy <-8)return 0;
+        result = (result*10) + dummy;  
+        }
+        return result;
+    }
+    public static void main(String args[]){
+	Scanner scanner = new Scanner(System.in);
+	int input = scanner.nextInt();
+	System.out.println(reverse(input));
+    }
+}
---
+++ 2021-04-03/InttoRoman.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/integer-to-roman/
+package Leetcode;
+import java.util.Scanner;
+
+class IntToRoman {
+    public String intToRoman(int num) {
+        
+        int numcode[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String numerals[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        StringBuilder result = new StringBuilder();
+        for(int tempIndex = 0; tempIndex < numcode.length; tempIndex++){
+            while(num >= numcode[tempIndex]){
+                result.append(numerals[tempIndex]);
+                num = num-numcode[tempIndex];
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String args[]){
+	Scanner scanner = new Scanner(System.in);
+	int input = scanner.nextInt();
+	System.out.println(intToRoman(input));
+    }
+}
