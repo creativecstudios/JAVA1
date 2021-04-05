9f3efa088bfb9fca6f26e2b4abbc6343dbdbb98b
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-05 12:30:47 +0530

    Add files via upload

--- 2021-04-03/Atoi.java
+++ 2021-04-03/Atoi.java
@@ -1,32 +1,40 @@
 //https://leetcode.com/problems/string-to-integer-atoi/
 package Leetcode;
 import java.util.Scanner;
-class Integerpalindrome {
-    public boolean isPalindrome(int x) {
-     
-        if(x==0){
-            return true;
+class Atoi{
+    public int myAtoi(String s) {
+        
+        if(s == null || s.isEmpty()){
+            return 0;
+        }
+        int length = s.length();
+        int i=0;
+        int sign = 1;
+        long result = 0;
+        
+        while(i < length &&s.charAt(i) == ' '){
+            ++i;
         }
         
-        if(x < 0 || x % 10 == 0){
-            return false;
+        if(i>=length){
+            return 0;
         }
         
-        int result = 0;
-        
-        while(x > result){
-           int dummy = x%10;
-            x /=10;
-            result = (result *10) + dummy;
+        if(s.charAt(i) == '+' || s.charAt(i) == '-'){
+            sign = s.charAt(i++) == '+' ? 1 : -1;
         }
         
-        if(x == result || x == result/10){
-            return true;
+        while(i<length && Character.isDigit(s.charAt(i))){
+            result = result * 10 + (s.charAt(i++) - '0');
+            
+            if(result*sign > Integer.MAX_VALUE || result*sign < Integer.MIN_VALUE){
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
         }
-        else{
-            return false;
-        }
+        
+        return (int)(sign*result);
     }
+
     public static void main(String args[]){
 	Scanner scanner = new Scanner(System.in);
 	int input = scanner.nextInt();
