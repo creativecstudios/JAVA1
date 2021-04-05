97e89a02d07231481af6d52ddf90fb879a6ac7a5
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-05 17:16:30 +0530

    Add files via upload

--- 2021-04-01/AddTwoNum.java
+++ 2021-04-01/AddTwoNum.java
@@ -1,31 +1,50 @@
 //https://leetcode.com/problems/add-two-numbers/
 package Leetcode;
-import java.util.Scanner;
 
-class LongestSubstring {
-        public static int lengthOfLongestSubstring(String s) {
-        int indx1 = 0;
-        int indx2 = 0;
-        int max = 0;
-        HashSet<Character> hashset = new HashSet();
-        while(indx2 < s.length()){
-            if(! hashset.contains(s.charAt(indx2))){
-                hashset.add(s.charAt(indx2));
-                indx2++;
-                max = Math.max(hashset.size(),max);
+class AddTwonum {
+         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        ListNode headlist = new ListNode();
+        ListNode listIterator = headlist;
+        
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        boolean carry = false;
+        
+        while(p1 != null || p2!= null){
+            int sum =0;
+            if(p1==null){
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            else if(p2==null){
+                sum+=p1.val;
+                p1 = p1.next;
             }
             else{
-                hashset.remove(s.charAt(indx1));
-                indx1++;
+                sum = p1.val + p2.val;
+                p1 = p1.next;
+                p2 = p2.next;
             }
+            if(carry){
+                sum++;
+            }
+            if(sum>=10){
+                sum %= 10;
+                carry = true;
+            }
+            else{
+                carry = false;
+            }
+            
+            ListNode result = new ListNode(sum);
+            listIterator.next = result;
+            listIterator =listIterator.next;
         }
-        return max;
+        if(carry){
+            listIterator.next = new ListNode(1);
+        }
+        return headlist.next;
+        
     }
-
-	public static void main(String args[]){
-		Scanner scanner = new Scanner(System.in);
-		String input = scanner.nextLine();
-		System.out.println(lengthofLongestSubstring(input));
-	}
-    
 }
