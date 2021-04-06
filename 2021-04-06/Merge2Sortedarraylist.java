9fe29c67b2fa0954fe13f051770ba726f83efd2d
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-06 20:06:49 +0530

    Add files via upload

---
+++ 2021-04-06/Merge2Sortedarraylist.java
@@ -0,0 +1,30 @@
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+package Leetcode;
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode result = dummy;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val <l2.val){
+                dummy.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+        if(l1 != null){
+            dummy.next = l1;
+        }
+        else{
+            dummy.next = l2;
+        }
+        return result.next;
+    }
+    
+}
