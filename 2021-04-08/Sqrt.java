69ab1a9e54a0350b1f5dba8634d424e5a7fc476f
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-08 23:06:29 +0530

    Update Sqrt.java

--- 2021-04-08/Sqrt.java
+++ 2021-04-08/Sqrt.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 
 class Sqrt{
 
-Example 1:
-
-Input: x = 4
-Output: 2
-Example 2:
-
-Input: x = 8
-Output: 2
-Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+// Example 1:
+// Input: x = 4
+// Output: 2
+	
+// Example 2:
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
 
 	public static int mysqrt(int num){
 		long start = 0; 
