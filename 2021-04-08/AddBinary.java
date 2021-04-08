be1254c81d1197a6333382f08270986f67d49703
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-08 20:20:08 +0530

    Update AddBinary.java

--- 2021-04-08/AddBinary.java
+++ 2021-04-08/AddBinary.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 
 class AddBinary {
 
-Example 1:
-
-Input: a = "11", b = "1"
-Output: "100"
-Example 2:
-
-Input: a = "1010", b = "1011"
-Output: "10101"
+// Example 1:
+// Input: a = "11", b = "1"
+// Output: "100"
+	
+// Example 2:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
 
 	public static String addBinary(String str1,String str2){
 		StringBuilder result = new StringBuilder();
