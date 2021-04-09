a6ffe515d33be10a7bd9cc8031e960584933e209
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-09 22:40:34 +0530

    Add files via upload

---
+++ 2021-04-09/ValidPalindrome.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/valid-palindrome/
+
+package Leetcode;
+class ValidPalindrome{
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+
+	public static boolean isPalindrome(String str){
+		int start = 0;
+		int end = str.length()-1;
+		while(start < end){
+
+			while(start < end && !Character.isLetterOrDigit(str.charAt(start))){
+				start++;
+			}
+			while(start < end && !Character.isLetterOrDigit(str.charAt(end))){
+				end--;
+			}
+			if(start < end && Character.toLowerCase(str.charAt(start++) != Character.toLowerCase(str.charAt(end--))){
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("input the string for palindrome");
+		String input = scanner.nextInt();
+		System.out.println(isPalindrome(input));
+	}
+}
