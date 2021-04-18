d3d1bc58490ebbc894d71dc519092a568d373570
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-18 13:21:05 +0530

    Update Flatten.java

--- 2021-04-17/Flatten.java
+++ 2021-04-17/Flatten.java
@@ -1,6 +1,25 @@
 //https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1225/
 
 class Flatten {
+    
+// Example 1:
+// Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+// Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+    
+// Example 2:
+// Input: head = [1,2,null,3]
+// Output: [1,3,2]
+// Explanation:
+// The input multilevel linked list is as follows:
+
+//   1---2---NULL
+//   |
+//   3---NULL
+      
+// Example 3:
+// Input: head = []
+// Output: []
+    
     public Node flatten(Node head) {
         
         Node temp = head;
