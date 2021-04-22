c1af2f10522fe1a10afb05b470976b8ee9b2aee7
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-22 11:48:52 +0530

    Update Flatten.java

--- 2021-04-17/Flatten.java
+++ 2021-04-17/Flatten.java
@@ -19,7 +19,43 @@ class Flatten {
 // Example 3:
 // Input: head = []
 // Output: []
-    
+	class Node{
+		int val;
+		Node next;
+		Node prev;
+		Node(int x){
+			this.val = x;
+		}
+	}
+	Node head = null;
+	
+	//addNode
+	public void addNode(int val) {
+		Node temp;
+		Node dummy;
+		temp = new Node(val);
+		if(head == null) {
+			head = temp;
+		}
+		else {
+			dummy = head;
+			while(dummy.next != null) {
+				dummy = dummy.next;
+			}
+			dummy.next = temp;
+			temp.prev = dummy;
+			temp.next = null;	
+		}	
+	}
+	//print node
+	public void printNode(Node head) {
+		Node temp = head;
+		while(temp != null) {
+			System.out.print(temp.val+" ");
+			temp = temp.next;
+		}
+	}
+    //flatten
     public Node flatten(Node head) {
         
         Node temp = head;
@@ -42,4 +78,15 @@ class Flatten {
         }
         return temp;
     }
+    
+    public static void main(String args[]) {
+		
+		DoublyLinkedList obj = new DoublyLinkedList();
+		obj.addNode(1);
+		obj.addNode(2);
+		obj.addNode(3);
+		obj.addNode(4);
+		obj.printNode(obj.head);
+        
+	}
 }
