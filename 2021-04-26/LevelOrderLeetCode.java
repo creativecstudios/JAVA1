7b5015e97748868ea5cdda162ce595f8175d603b
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-26 12:53:55 +0530

    Update LevelOrderLeetCode.java

--- 2021-04-26/LevelOrderLeetCode.java
+++ 2021-04-26/LevelOrderLeetCode.java
@@ -9,6 +9,18 @@ import java.util.Scanner;
 
 public class LevelOrderLeetCode {
 
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+
+// Example 2:
+// Input: root = [1]
+// Output: [[1]]
+	
+// Example 3:
+// Input: root = []
+// Output: []
+	
 	class Node{
 		int val;
 		Node left;
