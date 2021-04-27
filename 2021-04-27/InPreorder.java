f95ae8d53beae00c45939a0357e8fce1cd7a41f1
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-04-27 15:35:10 +0530

    Update InPreorder.java

--- 2021-04-27/InPreorder.java
+++ 2021-04-27/InPreorder.java
@@ -8,13 +8,13 @@ import java.util.Map;
 
 public class PreOrderInOrder {
 
-Example 1:
-Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
-Output: [3,9,20,null,null,15,7]
+// Example 1:
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
 
-Example 2:
-Input: preorder = [-1], inorder = [-1]
-Output: [-1]
+// Example 2:
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
  
 	class Node{
 		int val;
