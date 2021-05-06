8d4806b3d514508890dab601ac6e0fff961f3df8
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-05-06 22:52:57 +0530

    Update DeleteNode.java

--- 2021-05-06/DeleteNode.java
+++ 2021-05-06/DeleteNode.java
@@ -2,6 +2,25 @@
 package leetCode_Easy_118;
 
 public class DeleteNode {
+// 	Example 1:
+// Input: head = [4,5,1,9], node = 5
+// Output: [4,1,9]
+// Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+// 	Example 2:
+// Input: head = [4,5,1,9], node = 1
+// Output: [4,5,9]
+// Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+// 	Example 3:
+// Input: head = [1,2,3,4], node = 3
+// Output: [1,2,4]
+// 	Example 4:
+// Input: head = [0,1], node = 0
+// Output: [1]
+// 	Example 5:
+// Input: head = [-3,5,-99], node = -3
+// Output: [5,-99]
+ 
 
 	class Node{
 		int val;
