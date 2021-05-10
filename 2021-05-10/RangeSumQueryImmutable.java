bf5d2285d097ed173da2d19d1ff0dc34b1e9f066
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-05-10 16:00:45 +0530

    Add files via upload

---
+++ 2021-05-10/RangeSumQueryImmutable.java
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/range-sum-query-immutable/
+package leetCode_Easy_118;
+
+
+public class RangeSumQueryImmutable {
+
+	//pseudo code
+	//Begin
+	//Declare arr[]
+	//Input arr[]
+	//Declare sum = 0
+	//Initialize i = 0;
+	//For i 0 to arr.length
+	//sum = sum + arr[i]
+	//End For
+	//End
+	
+	static private int arr[];
+	
+	public static void numarr(int nums[]) {
+		arr = nums;
+	}
+	
+	public static int sum (int left,int right) {
+		int sum = 0;
+		for(int temp = left; temp <= right; temp++) {
+			sum += arr[temp];
+		}
+		return sum;
+	}
+	
+	public static void main(String args[]) {
+		int arr[] = {1,3,13,1,3,24};
+		numarr(arr);
+		int result = sum(0,2);
+		System.out.println(result);
+	}
+	
+}
