c2636e14fe014997f409bbe28333df283eda974e
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-05-13 13:57:26 +0530

    Update firstUniqueChar.java

--- 2021-05-13/firstUniqueChar.java
+++ 2021-05-13/firstUniqueChar.java
@@ -1,5 +1,19 @@
+//https://leetcode.com/problems/first-unique-character-in-a-string/
 package LeetCode383;
 
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+
+// Input: s = "loveleetcode"
+// Output: 2
+// Example 3:
+
+// Input: s = "aabb"
+// Output: -1
+	
 public class firstUniqueChar {
 
 	//pseudoCode
